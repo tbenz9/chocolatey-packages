5c8926e320b45ed7b2ed8f38b91716ff048c944f
Thomas Bennett <tbenz9@gmail.com>
2014-10-31 13:36:09 -0600

    Updated deluge to version 1.3.10

--- deluge/tools/chocolateyInstall.ps1
+++ deluge/tools/chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyPackage 'deluge' 'exe' '/S' 'http://download.deluge-torrent.org/windows/deluge-1.3.7-win32-setup.exe'
+Install-ChocolateyPackage 'deluge' 'exe' '/S' 'http://download.deluge-torrent.org/windows/deluge-1.3.10-win32-setup.exe'
